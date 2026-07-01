446ad630d53027c50607e45dc2957eef44c350f5
sohail-hasan-11 <sohailhasan211@gmail.com>
2026-07-01 12:20:51 +0600

    Annoteted practice for first api

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import Annotated
 app = FastAPI()
 
 name : str | None = None
 print(name)
+name_annotated : Annotated[str | None, Query(max_length = 5)] = None
+name_annotated = "Abdullah"
+print(name_annotated)
 
+@app.get("/name")
+def print_name(name : Annotated[str | None, Query(min_length = 3, max_length = 5)] = None):
+    # print(f"My name is : {name}")
+    return f"My name is {name}"
 
-# For auto reload the main file
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-
+# Annotated validate a parameter with first api it also check query
+# print_name("Sohail")
+# print_name("Abdullah Al Sourov")
