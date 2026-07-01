5b9fd36dd0a29a62fcdbc4e9f0f4f0234854ee46
sohail-hasan-11 <sohailhasan211@gmail.com>
2026-07-01 11:42:16 +0600

    The detailes that I do today

--- main.py
+++ main.py
@@ -1,61 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Annotated
-# app = FastAPI()
-# Now github is ready for the code
+app = FastAPI()
 
-# name : str | None = None
-
-# def print_name(name : str | None = None):
-#     print(f"My name is = {name}")
-# print_name()
-# print_name("Abdullah")
-
-# class person(BaseModel):
-#     name : str 
-#     age : int
-#     address : str 
-
-# def print_inf(p : person):
-#     print(f"Name is {p.name} age {p.age} address {p.address}")
-
-# print_inf(person("name" : "Sohail", "age" : 10, "address" : "Dhaka"))
-# print_inf({"Abdullah", 25, "Kurigram"})
-
-# class codeforces(BaseModel):
-#     handle : str
-#     rating : int | None
-
-# @app.post("/register")
-# def codeforces_user(user : codeforces):
-#     return{
-#         "user" : user,
-#         "docs" : f"User has post something and I get the information on user calss that  I can be processed"
-#     }
+name : str | None = None
+print(name)
 
 
-# @app.get("/")
-# def func() :
-#     return "Alhamdulillah first api learn"
+# For auto reload the main file
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
-# @app.get("/home")
-# def obj():
-#     return {
-#         "name" : "Abdullah",
-#         "age" : 25,
-#         "cgp" : 3.1
-#     }
-# @app.get("/user/{user_id}")
-# def get_user(user_id : int):
-#     return{
-#         "name" : f"Hello user id {user_id}",
-#         "user" : user_id,
-#     }
-# @app.get("/item")
-# def get_user(name : str, price : int):
-#     return{
-#         "name" : name,
-#         "price" : price,
-#         "massege" : f"product {name}'s prize is {price}"
-#     }
 
