0cb2feac0a53dfa95ba0fd52a4cd8c6284b254a2
sohail-hasan-11 <sohailhasan211@gmail.com>
2026-07-01 21:45:34 +0600

    I can declear a array in python like number : list[int] = [] it is a array which will only contain integer value

--- main.py
+++ main.py
@@ -3,17 +3,40 @@ from pydantic import BaseModel
 from typing import Annotated
 app = FastAPI()
 
-name : str | None = None
+name : list[str] = []
+name.append("Abdullah")
+name.append("Sohail")
 print(name)
-name_annotated : Annotated[str | None, Query(max_length = 5)] = None
-name_annotated = "Abdullah"
-print(name_annotated)
 
-@app.get("/name")
-def print_name(name : Annotated[str | None, Query(min_length = 3, max_length = 5)] = None):
-    # print(f"My name is : {name}")
-    return f"My name is {name}"
+number : list[int] = []
+for i in range(1, 10):
+    number.append(i)
+
+print(number)
+
+# name : str | None = None
+# print(name)
+# name_annotated : Annotated[str | None, Query(max_length = 5)] = None
+# name_annotated = "Abdullah"
+# print(name_annotated)
+
+# @app.get("/name")
+# def print_name(name : Annotated[str | None, Query(min_length = 3, max_length = 5)] = None):
+#     # print(f"My name is : {name}")
+#     return f"My name is {name}"
 
 # Annotated validate a parameter with first api it also check query
 # print_name("Sohail")
 # print_name("Abdullah Al Sourov")
+# print("Terminal is hot reload")
+# print("After auto save terminal is do hot reload and the output in shown on terminal")
+# @app.get("/")
+# def number_validation(num : Annotated[list[str] | None, Query()] = None):
+    # query_item = {"arr" : num}
+    # # dictionary update with value and add new value
+    # query_item.update({
+    #     "name" : "Add new value",
+    #     "arr" : num
+    # })
+    
+    # return query_item
